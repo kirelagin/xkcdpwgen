8f777d163500eaeddbc67d69ae91b371183fd4a9
Kirill Elagin <kirelagin@gmail.com>
2014-02-17 13:55:07 +0400

    Bug in determining word length

--- xkcdpwgen/generator.py
+++ xkcdpwgen/generator.py
@@ -6,11 +6,12 @@ def generate_(wordlist, count=4, min_len=4, max_len=10):
     words = [""] * 4
 
     for i, w in enumerate(wordlist):
+        w = w.strip()
         if min_len <= len(w) <= max_len:
             words_matched += 1
             for j in range(count):
                 if random.randint(1, words_matched) == 1:
-                    words[j] = w.strip()
+                    words[j] = w
 
     return (words, words_matched)
 
